b29f9c9f36a199c60cf4abe96e01dc761bc92f3e
thiagobc98 <thiagocabral98.tc@gmail.com>
2026-06-15 17:38:25 -0300

    feat: configure cloud storage path and github actions pipeline

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,5 @@
 import pandas as pd 
 
-# Extrai os dados do csv da pasta raw
+# Extrai os dados do csv da pasta raw no Cloud Storage
 def extract_data():
-    return pd.read_csv(fr'C:\Users\Thiago\Desktop\GitHub\projeto_tecnico_localiza\data\raw\df_fraud_credit.csv')
-
-print(extract_data().head())
+    return pd.read_csv('gs://landing-raw/df_fraud_credit.csv')
